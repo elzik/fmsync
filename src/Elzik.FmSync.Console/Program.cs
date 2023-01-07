57754971580a0265250a6badee2c88d31753eed9
elzik <23397871+elzik@users.noreply.github.com>
2023-01-07 15:50:14 +0000

    Enumerate files and get creation date

--- src/Elzik.FmSync.Console/Program.cs
+++ src/Elzik.FmSync.Console/Program.cs
@@ -1,3 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+string searchPath;
+if (args.Length == 0)
+{
+    searchPath = Directory.GetCurrentDirectory();
+}
+else
+{
+    searchPath = args[0];
+}
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine($"Syncing Markdown files in {searchPath}");
+
+var files = Directory.EnumerateFiles(searchPath, "*.md", new EnumerationOptions()
+{
+    MatchCasing = MatchCasing.CaseInsensitive,
+    RecurseSubdirectories = true
+});
+
+foreach (var file in files)
+{
+    var markdownFileInfo = new FileInfo(file);
+
+    var fileCreatedDate = markdownFileInfo.CreationTimeUtc;
+
+    Console.WriteLine(file);
+    Console.WriteLine($"\tFile created date: {fileCreatedDate}");
+}
