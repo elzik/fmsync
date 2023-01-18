336225ea716b5d16e867d36773b65e447279899d
elzik <23397871+elzik@users.noreply.github.com>
2023-01-18 21:40:49 +0000

    Improve mock logger to better handle original data types rather than just strings

--- tests/Elzik.FmSync.Application.Tests.Unit/MockLogger.cs
+++ tests/Elzik.FmSync.Application.Tests.Unit/MockLogger.cs
@@ -1,19 +1,34 @@
-﻿using Microsoft.Extensions.Logging;
-
-namespace Elzik.FmSync.Application.Tests.Unit
+﻿namespace Elzik.FmSync.Application.Tests.Unit
 {
+    using Microsoft.Extensions.Logging;
+
     // This class is necessary because mocking an ILogger will not work; Further explanation here:
-    // https://github.com/nsubstitute/NSubstitute/issues/597#issuecomment-653555567
+    // https://github.com/nsubstitute/NSubstitute/issues/597#issuecomment-1081422618
 
     public abstract class MockLogger<T> : ILogger<T>
     {
-        void ILogger.Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
-            => Log(logLevel, formatter(state, exception));
+        void ILogger.Log<TState>(LogLevel logLevel, EventId eventId, TState state, 
+            Exception? exception, Func<TState, Exception?, string> formatter)
+        {
+            var unboxed = (IReadOnlyList<KeyValuePair<string, object>>)state!;
+            var message = formatter(state, exception);
 
-        public abstract void Log(LogLevel logLevel, string message);
+            Log();
+            Log(logLevel, message, exception);
+            Log(logLevel, unboxed.ToDictionary(k => 
+                k.Key, v => v.Value), exception);
+        }
 
-        public virtual bool IsEnabled(LogLevel logLevel) => true;
+        public abstract void Log();
 
-        public abstract IDisposable BeginScope<TState>(TState state);
+        public abstract void Log(LogLevel logLevel, string message, Exception? exception = null);
+
+        public abstract void Log(LogLevel logLevel, IDictionary<string, object> state, Exception? exception = null);
+
+        public virtual bool IsEnabled(LogLevel logLevel)
+        {
+            return true;
+        }
+        public abstract IDisposable? BeginScope<TState>(TState state) where TState : notnull;
     }
 }
