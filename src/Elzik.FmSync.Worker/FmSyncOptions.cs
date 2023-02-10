85f5ce8cfc3cfeb83027495c2f8537f63ee69e4b
elzik <23397871+elzik@users.noreply.github.com>
2023-02-10 12:56:25 +0000

    Ensure that list is never null

--- src/Elzik.FmSync.Worker/FmSyncOptions.cs
+++ src/Elzik.FmSync.Worker/FmSyncOptions.cs
@@ -2,6 +2,6 @@
 {
     public class FmSyncOptions
     {
-        public IEnumerable<string> WatchedDirectoryPaths { get; set; }
+        public IEnumerable<string> WatchedDirectoryPaths { get; set; } = new List<string>();
     }
 }
