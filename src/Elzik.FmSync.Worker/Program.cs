b87eae294670534d2ba2af5c4c8b57b4849b2d39
elzik <23397871+elzik@users.noreply.github.com>
2023-09-23 19:48:26 +0100

    Log service to file

--- src/Elzik.FmSync.Worker/Program.cs
+++ src/Elzik.FmSync.Worker/Program.cs
@@ -2,10 +2,13 @@ using Elzik.FmSync;
 using Elzik.FmSync.Domain;
 using Elzik.FmSync.Infrastructure;
 using Elzik.FmSync.Worker;
+using Serilog;
 using Thinktecture.IO.Adapters;
 using Thinktecture.IO;
 
 var host = Host.CreateDefaultBuilder(args)
+    .UseSerilog((ctx, lc) => lc
+        .ReadFrom.Configuration(ctx.Configuration))
     .ConfigureServices((hostContext, services) =>
     {
         services.AddSingleton<IMarkdownFrontMatter, MarkdownFrontMatter>();
@@ -14,14 +17,18 @@ var host = Host.CreateDefaultBuilder(args)
         services.Configure<FmSyncOptions>(hostContext.Configuration.GetSection("FmSyncOptions"));
         services.Configure<FileSystemOptions>(hostContext.Configuration.GetSection("FileSystemOptions"));
         services.Configure<FrontMatterOptions>(hostContext.Configuration.GetSection("FrontMatterOptions"));
+#if IS_WINDOWS_OS
+        services.AddWindowsService(options =>
+        {
+            options.ServiceName = "fmsync";
+        });
+#endif
         services.AddHostedService<FmSyncWorker>();
     })
     .ConfigureAppConfiguration((_, config) =>
     {
         config.AddJsonFile("appSettings.json", true, true);
     })
-    .ConfigureLogging((context, config) => 
-        config.AddConfiguration(context.Configuration.GetSection("Logging")))
     .Build();
 
 host.Run();
