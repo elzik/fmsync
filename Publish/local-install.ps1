127562eaa002ad00808f2095b7e77d18f59d1cd5
elzik <23397871+elzik@users.noreply.github.com>
2023-10-06 20:47:12 +0100

    Add Windows installer

--- Publish/local-install.ps1
+++ Publish/local-install.ps1
@@ -6,8 +6,11 @@ Stop-Service -Name 'fmsync'
 Remove-Service -Name 'fmsync'
 
 robocopy $PSScriptRoot\output\Elzik.FmSync.Worker\win-x64 "c:\program files\fmsync\worker" /e
-(get-content "c:\program files\fmsync\worker\appSettings.json") | foreach-object {$_ -replace '"WatchedDirectoryPaths": \[\]', '"WatchedDirectoryPaths": ["C:\\Users\\justin.elzik\\Obsidian"]'} | set-content "c:\program files\fmsync\worker\appSettings.json"
 
+(get-content "c:\program files\fmsync\worker\appSettings.json") `
+	| foreach-object {$_ -replace '"WatchedDirectoryPaths": \[\]', '"WatchedDirectoryPaths": ["C:\\Users\\justin.elzik\\Obsidian"]'} 
+	| foreach-object {$_ -replace '"MinimumLevel": "Information"', '"MinimumLevel": "Debug"'} `
+	| set-content "c:\program files\fmsync\worker\appSettings.json"
 
 New-Service -Name 'fmsync' `
 	-BinaryPathName 'c:\program files\fmsync\worker\Elzik.FmSync.Worker.exe' `
