14a65e9365569252214413bf09da6ab67c9c2299
elzik <23397871+elzik@users.noreply.github.com>
2023-10-20 14:33:37 +0100

    Build scripts from any directory

--- Build/build-and-test.ps1
+++ Build/build-and-test.ps1
@@ -1,8 +1,8 @@
-dotnet test `
+dotnet test $PSScriptRoot\..\Elzik.FmSync.sln `
 	-c Release `
 	--verbosity normal `
 	-p:CollectCoverage=true `
-	-p:CoverletOutput=TestResults/coverage.opencover.xml `
+	-p:CoverletOutput=$PSScriptRoot/../tests/TestResults/coverage.opencover.xml `
 	-p:CoverletOutputFormat=opencover
 
 dotnet tool update `
@@ -10,6 +10,6 @@ dotnet tool update `
 	--version 5.1.8
 
 reportgenerator `
-	"-reports:tests/Elzik.FmSync.Application.Tests.Unit/TestResults/coverage.opencover.xml;tests/Elzik.FmSync.Infrastructure.Tests.Integration/TestResults/coverage.opencover.xml;" `
-	"-targetdir:tests/TestResults" `
+	"-reports:$PSScriptRoot/../tests/Elzik.FmSync.Application.Tests.Unit/TestResults/coverage.opencover.xml;$PSScriptRoot/../tests/Elzik.FmSync.Infrastructure.Tests.Integration/TestResults/coverage.opencover.xml;" `
+	"-targetdir:$PSScriptRoot/../tests/TestResults" `
 	"-reporttypes:Badges"
