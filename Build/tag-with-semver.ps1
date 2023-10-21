5cfe08a5c85d1d2c9cc70523ef2b76cb91b43fa0
elzik <23397871+elzik@users.noreply.github.com>
2023-10-21 22:01:38 +0100

    Improve script formatting

--- Build/tag-with-semver.ps1
+++ Build/tag-with-semver.ps1
@@ -1,6 +1,9 @@
 dotnet tool update --global GitVersion.Tool
+
 $semVer = (dotnet-gitversion | ConvertFrom-Json).SemVer
 $tag = "v$semVer"
+
 git tag $tag -f
 git push --tags
+
 Write-Output "Tagged with $tag"
