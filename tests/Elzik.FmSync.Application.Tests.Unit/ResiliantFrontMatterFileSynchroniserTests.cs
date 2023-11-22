a0cf9c66a304d7542792c345ee50f533312d5261
elzik <23397871+elzik@users.noreply.github.com>
2023-11-22 14:02:34 +0000

    Add ResiliantFrontMatterFileSynchroniser tests

---
+++ tests/Elzik.FmSync.Application.Tests.Unit/ResiliantFrontMatterFileSynchroniserTests.cs
@@ -0,0 +1,43 @@
+﻿using AutoFixture;
+using NSubstitute;
+using Polly;
+using Polly.Registry;
+using Xunit;
+
+namespace Elzik.FmSync.Application.Tests.Unit
+{
+    public class ResiliantFrontMatterFileSynchroniserTests
+ {
+        private readonly Fixture _fixture;
+        private readonly IFrontMatterFileSynchroniser _mockFrontMatterFileSynchroniser;
+        private readonly ResiliencePipelineProvider<string> _emptyResiliencePipelineProvider;
+
+        public ResiliantFrontMatterFileSynchroniserTests()
+        {
+            _fixture = new Fixture();
+
+            _mockFrontMatterFileSynchroniser = Substitute.For<IFrontMatterFileSynchroniser>();
+
+            _emptyResiliencePipelineProvider = Substitute.For<ResiliencePipelineProvider<string>>();
+            _emptyResiliencePipelineProvider
+                .GetPipeline("retry-5-times")
+                .Returns(ResiliencePipeline.Empty);
+        }
+
+        [Fact]
+        public void SyncCreationDate_IsCalled_Resiliently()
+        {
+            // Arrange
+            var testFilePath = _fixture.Create<string>();
+
+            // Act
+            var resiliantFrontMatterFileSynchroniser = new ResiliantFrontMatterFileSynchroniser(
+                _mockFrontMatterFileSynchroniser,
+                _emptyResiliencePipelineProvider);
+            resiliantFrontMatterFileSynchroniser.SyncCreationDate(testFilePath);
+
+            // Assert
+            _mockFrontMatterFileSynchroniser.Received(1).SyncCreationDate(testFilePath);
+        }
+    }
+}
