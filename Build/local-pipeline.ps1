12d7a99d683037f2599d44d6e456ef7c4525e9b7
elzik <23397871+elzik@users.noreply.github.com>
2024-02-24 14:00:31 +0000

    Ensure non-zero exit code when a PoerShell script fails

--- Build/local-pipeline.ps1
+++ Build/local-pipeline.ps1
@@ -1,4 +1,7 @@
+Import-Module $(Resolve-Path ./Build/Test-ExitCode.psm1)
 $ErrorActionPreference = "Stop"
 
 & $PSScriptRoot/build-and-test.ps1
+Test-ExitCode
 & $PSScriptRoot/build-windows-installer.ps1
+Test-ExitCode
