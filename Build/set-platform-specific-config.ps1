17596b45a17a3439df6768e92a0eb8aa01448691
elzik <23397871+elzik@users.noreply.github.com>
2024-04-04 20:15:33 +0100

    Improve error handling and logging in config script

--- Build/set-platform-specific-config.ps1
+++ Build/set-platform-specific-config.ps1
@@ -5,6 +5,8 @@ Param(
     [String] $operatingSystem
 )
 
+$ErrorActionPreference = "Stop"
+
 if ($operatingSystem -eq 'Windows_NT')
 {
     $platformSpecificLogPath = 'C:/ProgramData/Elzik/fmsync'
@@ -14,7 +16,11 @@ else
     $platformSpecificLogPath = '~/Library/Logs/Elzik/fmsync'
 }
 
+Write-Output "Platform specific log path: $platformSpecificLogPath"
+
 $appSettingsPath = "$outputDirectory/appSettings.json"
+
+Write-Output "appSettings Path: $appSettingsPath"
 
 (Get-Content $appSettingsPath).Replace('[PLATFORM_SPECIFIC_LOG_PATH]', $platformSpecificLogPath) `
     | Set-Content $appSettingsPath
