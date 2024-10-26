b665dba4df9cb06a71f94e402a60f6940bf0bfc6
elzik <23397871+elzik@users.noreply.github.com>
2024-10-26 11:00:05 +0100

    Fix all installs of GitVersion to major version 6

--- Build/build-osx-installer.ps1
+++ Build/build-osx-installer.ps1
@@ -72,7 +72,7 @@ Compress-Archive `
 	-Force
 Test-ExitCode
 
-dotnet tool update --global GitVersion.Tool
+dotnet tool update --global GitVersion.Tool --version 6.*
 Test-ExitCode
 
 $SemVer = (dotnet-gitversion | ConvertFrom-Json).SemVer
